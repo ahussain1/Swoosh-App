77b5764d69de9059a68708818215c35d7fe5d2e5
Arif Hussain <hussain.arif33@gmail.com>
2018-10-01 00:31:15 -0400

    Welcome Screen Layout

--- app/src/main/java/com/arifh/swoosh/WelcomActivity.kt
+++ app/src/main/java/com/arifh/swoosh/WelcomActivity.kt
@@ -7,6 +7,6 @@ class WelcomActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcom)
+        setContentView(R.layout.activity_welcome)
     }
 }
