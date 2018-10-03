20d2a27d44e960243a41dcad8bef2e52d554a8f5
Arif Hussain <hussain.arif33@gmail.com>
2018-10-02 23:49:37 -0400

    Intents and Context

--- app/src/main/java/com/arifh/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/arifh/swoosh/WelcomeActivity.kt
@@ -1,12 +1,21 @@
 package com.arifh.swoosh
-
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) { //bundles contains key-value paris
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent) //Implicit IntentAction vs Explicit Intent
+        }
+
     }
+
+
 }
