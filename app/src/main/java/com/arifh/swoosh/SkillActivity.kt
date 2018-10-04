df4c51b003f0d21a7dc4d2a3cc0f8efbf8f1b2c3
Arif Hussain <hussain.arif33@gmail.com>
2018-10-03 22:15:51 -0400

    Skill selection layout

---
+++ app/src/main/java/com/arifh/swoosh/SkillActivity.kt
@@ -0,0 +1,12 @@
+package com.arifh.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.skill_activity)
+    }
+}
